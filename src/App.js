1c95193038d7da9d211a3f811572d4675abc489b
xinnxz <luthfifahmi.alv@gmail.com>
2023-10-30 10:20:10 +0700

    edit tipis tipis

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className='basis-[12] h-[100vh] border'>
+        
+      </div>
+      <div className='basis-[88] border'>d
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='bg-[#443bee]'>Sidebar</div>
+  )
+}
+
+export default Sidebar
